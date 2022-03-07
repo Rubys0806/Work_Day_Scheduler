705f7a95f9a6fd8de1ff675cd524518427efb172
Rubys0806 <rubys0806@gmail.com>
2022-03-06 19:32:24 -0700

    Added timeblock colors based on past, present and future time

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,28 @@
 // Current Date
 $("#currentDay").html(moment().format("dddd, MMMM Do YYYY"));
+
+//Change timeblock colors
+    //Current Hour
+    var hourNOW = moment().hours();
+    //var hourNOW = 11;
+
+   //Loops through each timeblock to see if its past, present or future
+   $(".time-block").each(function () {
+       var blockHour = parseInt(this.id);
+
+       if (blockHour < hourNOW) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+        }
+        else if (blockHour === hourNOW) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        }
+        else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        }
+    })
